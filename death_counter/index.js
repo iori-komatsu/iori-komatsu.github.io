887345620393b801e3df8cbc1f8dc0fc073bcf34
Iori Komatsu <iori.komatsu@gmail.com>
2025-01-01 00:59:29 +0900

    Add death_counter

---
+++ death_counter/index.js
@@ -0,0 +1,78 @@
+// "12345" を "12,345" のようにカンマで区切る
+function formatNumberWithCommas(number) {
+    let s = number.split('').reverse().join('');
+    s = s.replace(/(\d{3})(?=\d)/g, '$1,');
+    return s.split('').reverse().join('');
+}
+
+function drawDigits(canvas, ctx, digitsImage, iconImage, inputValue, dryrun) {
+    if (!dryrun)
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    if (!/^[0-9]+$/.test(inputValue))
+        return 0;
+
+    inputValue = formatNumberWithCommas(inputValue);
+
+    if (!dryrun)
+        ctx.drawImage(iconImage, 0, 0);
+
+    const digitWidth = digitsImage.width / 11;
+    let dx = iconImage.width;
+    for (let i = 0; i < inputValue.length; i++) {
+        const digit = inputValue[i];
+        const digitIndex = digit == ',' ? 10 : parseInt(digit);
+        const w = digit == ',' ? digitWidth * 2.0 / 3.0 : digitWidth;
+        if (!dryrun) {
+            ctx.drawImage(
+                digitsImage,
+                digitIndex * digitWidth, // sx
+                0, // sy
+                w, // sw
+                digitsImage.height, // sh
+                dx, // dx
+                0, // dy
+                w, // dw
+                digitsImage.height // dh
+            );
+        }
+        dx += w;
+    }
+
+    return dx;
+}
+
+window.onload = () => {
+    const canvas = document.getElementById("canvasDeathCounter");
+    const ctx = canvas.getContext("2d");
+    const countInput = document.getElementById("inputCount");
+    const downloadButton = document.getElementById("downloadButton");
+
+    const digitsImage = new Image();
+    digitsImage.src = "digits.webp";
+
+    const iconImage = new Image();
+    iconImage.src = "icon.webp";
+
+    countInput.addEventListener("input", () => {
+        const inputValue = countInput.value.trim();
+        const w = drawDigits(canvas, ctx, digitsImage, iconImage, inputValue, true);
+        if (w > 0) {
+            countInput.style.color = "";
+            canvas.width = w;
+        } else {
+            // 何らかの理由で描画できない。テキストの文字色を変えてユーザーに伝える。
+            countInput.style.color = "#e9171e";
+        }
+        drawDigits(canvas, ctx, digitsImage, iconImage, inputValue, false);
+    })
+
+    downloadButton.addEventListener("click", () => {
+        const dataUrl = canvas.toDataURL("image/png");
+
+        const link = document.createElement("a");
+        link.href = dataUrl;
+        link.download = "DC" + countInput.value.trim() + ".png";
+        link.click();
+    })
+}
